043e246a8208a6b99932db883b574e1061c7304b
14140079 Колупаев Всеволод Михайлович <kovm14a@econ.msu.ru>
2019-11-05 12:06:40 +0300

    GOD

---
+++ GOD.R
@@ -0,0 +1,9 @@
+generate_data<- function() {
+
+  x1 <- rnorm(100, 0, 1)
+  x2 <- rnorm(100, 0, 1)
+  x3 <- rnorm(100, 0, 1)
+  y <- 2*x1 + 3*x2 + 4*x3
+  data <- data.frame(x1=x1, x2=x2, x3=x3, y=2*x1+3*x2+4*x3)
+  return(data)
+}
