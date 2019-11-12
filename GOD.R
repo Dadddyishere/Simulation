befdf495886e3381b099dd90a92f499f88e1e437
bafk95 <57386426+bafk95@users.noreply.github.com>
2019-11-12 10:27:39 +0300

    Update GOD.R

--- GOD.R
+++ GOD.R
@@ -1,10 +1,9 @@
 #Kolupaev Vsevolod is Lord
-generate_data<- function() {
-
-  x1 <- rnorm(100, 0, 1)
-  x2 <- rnorm(100, 0, 1)
-  x3 <- rnorm(100, 0, 1)
-  y <- 2*x1 + 3*x2 + 4*x3
-  data <- data.frame(x1=x1, x2=x2, x3=x3, y=2*x1+3*x2+4*x3)
+generate_data<- function(n, a1 = 2, a2 = 3, a3 = 4) {
+  x1 <- rnorm(n, 0, 1)
+  x2 <- rnorm(n, 0, 1)
+  x3 <- rnorm(n, 0, 1)
+  y <- a1*x1 + a2*x2 + a3*x3
+  data <- data.frame(x1=x1, x2=x2, x3=x3, y=y)
   return(data)
 }
